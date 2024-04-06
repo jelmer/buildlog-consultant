f2e4aa9ccca721eb31f3024febde1578c8e18036
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-04-06 22:01:00 +0100

    Ignore ruff error

--- buildlog_consultant/apt.py
+++ buildlog_consultant/apt.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-import re
+import re  # noqa: I001
 from typing import Optional, TypedDict
 
 import yaml
